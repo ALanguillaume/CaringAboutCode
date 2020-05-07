b6b9cd456371be26a5318fd2061b87e310d4ca7f
Languillaume, Antoine <antoine.languillaume@wur.nl>
2020-05-07 15:26:13 +0200

    include docs to display slides

---
+++ build_project.R
@@ -0,0 +1,8 @@
+
+rmarkdown::render("slides/Caring_About_Code.Rmd")
+
+file.copy(from = "slides/Caring_About_Code.html",
+					to = "docs/Caring_About_Code.html")
+
+file.rename(from = "docs/Caring_About_Code.html", 
+						to = "docs/index.html")
